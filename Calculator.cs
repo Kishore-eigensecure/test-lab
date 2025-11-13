0117440fecbd61ea9ea24519ca78150b3d62d630
abhishek-eigensecure <abhishek.samuel@eigensecure.com>
2025-11-13 18:59:34 +0530

    some commit

--- Calculator.cs
+++ Calculator.cs
@@ -18,6 +18,14 @@ namespace GitPracticeLab
         {
             return a % b;
 
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a % b;
+
+        }
+
         // Try adding new methods here (e.g., Subtract, Divide)
     }
 }
